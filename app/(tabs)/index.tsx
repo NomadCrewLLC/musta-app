7bc701bcd56f29620e8e3396b03b2cb27164d33c
Jyle Vergara <jylevergara@gmail.com>
2024-10-08 01:12:13 -0400

    show translation list from json

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,64 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  VirtualizedList,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from 'react-native';
+import data from '@/app/data/phrases.json'
+import { FlashCard } from '@/components/FlashCard';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const phrases = data.phrases;
+
+type ItemData = {
+  id: string;
+  title: string;
+};
+
+type PhraseData = {
+  phrases: [ {phrase: string, translation: string}];
+}
+
+const getPhrase = (data: PhraseData, index: number) => ({
+  id: Math.random().toString(12).substring(0),
+  phrase: phrases[index].phrase,
+  translation: phrases[index].translation,
+});
+
+const getPhraseCount = () => data.phrases.length;
+const item = data.phrases;
+
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.container}>
+      <VirtualizedList
+        initialNumToRender={4}
+        renderItem={({item}) => <FlashCard title={item.phrase} subtitle={item.translation} />}
+        keyExtractor={item => item.id}
+        getItemCount={getPhraseCount}
+        getItem={getPhrase}
+      />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  item: {
+    backgroundColor: '#fff5cc',
+    height: 150,
+    justifyContent: 'center',
+    marginVertical: 8,
+    marginHorizontal: 16,
+    padding: 20,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  title: {
+    fontSize: 32,
   },
 });
---
+++ components/FlashCard.tsx
@@ -0,0 +1,36 @@
+import {
+  Text,
+  View,
+  StyleSheet,
+} from "react-native";
+
+type FlasCardProps = {
+  title: string;
+  subtitle: string;
+};
+
+export function FlashCard({ title, subtitle}: FlasCardProps) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.subtitle}>{subtitle}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: 'antiquewhite',
+    height: 150,
+    justifyContent: "center",
+    marginVertical: 8,
+    marginHorizontal: 16,
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+  },
+  subtitle: {
+    fontSize: 24,
+  },
+});
